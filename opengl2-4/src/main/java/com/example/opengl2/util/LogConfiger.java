3acf6d40556b08724fddb738b96ee8bb18c4de59
hua.pang <2668873033@qq.com>
2017-10-27 18:01:28 +0800

    调整屏幕的宽高比

---
+++ opengl2-4/src/main/java/com/example/opengl2/util/LogConfiger.java
@@ -0,0 +1,33 @@
+package com.example.opengl2.util;
+
+import android.util.Log;
+
+/**
+ * Created by hua.pang on 2017/10/24.
+ */
+
+public class LogConfiger{
+    public static boolean ON = true;
+    private static String TAG = "PH_opengl2:";
+
+    public static int v(String tag, String msg) {
+        return Log.v(TAG+tag,msg);
+    }
+
+    public static int d(String tag, String msg) {
+        return Log.d(TAG+tag,msg);
+    }
+
+    public static int i(String tag, String msg) {
+        return Log.i(TAG+tag,msg);
+    }
+
+    public static int w(String tag, String msg) {
+        return Log.w(TAG+tag,msg);
+    }
+
+    public static int e(String tag, String msg) {
+        return Log.e(TAG+tag,msg);
+    }
+
+}
---
+++ opengl2-4/src/main/java/com/example/opengl2/view/Circle.java
@@ -0,0 +1,12 @@
+package com.example.opengl2.view;
+
+/**
+ * Created by hua.pang on 2017/10/26.
+ */
+
+public class Circle {
+
+
+
+
+}
